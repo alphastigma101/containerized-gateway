5efc49ec7e697a9bbbc33d59d17dd5121b038b16
Logan Welty <12logan.welty@gmail.com>
2024-07-07 18:55:30 -0700

    Created the default databases and linked them together. I am now having issues with loading up the .css file which for some reason I can't get it to work

--- app/gateway/models.py
+++ app/gateway/models.py
@@ -1,3 +1,25 @@
+"""
+    You can view the user manual from here: https://docs.djangoproject.com/en/5.0/topics/db/models/
+    Which shows you how to fully utilize Django's model api method 
+"""
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    '''
+        Represents a user in the application.
+    '''
+    username = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.username
+
+class Logging(models.Model):
+    '''
+        Represents a logging entry associated with a user.
+    '''
+    issue = models.CharField(max_length=30)
+    date = models.CharField(max_length=8)  # Must be in valid date format
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.issue} - {self.user.username}'
--- app/gateway/urls.py
+++ app/gateway/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home", views.home, name="home")
+    path("home", views.home, name="home"),
+    path("login", views.login, name="login"),
+    path("update_data", views.update_data, name="update_data"),
+    path("delete_data", views.delete_data, name="delete_data"),
+    path("query_data", views.query_data, name="query_data")
 ]
