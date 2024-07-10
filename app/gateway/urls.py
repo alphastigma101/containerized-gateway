bd7cabe8387b677940600a8e3dcce67f22a945db
Logan Welty <12logan.welty@gmail.com>
2024-07-09 20:09:33 -0700

    Fixed the bug but now I have to re-implement the database getting created and launched when the app is launched

--- app/gateway/urls.py
+++ app/gateway/urls.py
@@ -20,6 +20,7 @@ from gateway import views  # Import views module from current directory
 urlpatterns = [
     path("home", views.home, name="home"),
     path("update_data", views.update_data, name="update_data"),
+    path("upload_data", views.upload_data, name="upload_data"),
     path("delete_data", views.delete_data, name="delete_data"),
     path("query_data", views.query_data, name="query_data"),
     path("bug_report", views.bug_report, name="bug_report"),
--- app/gateway/views.py
+++ app/gateway/views.py
@@ -5,10 +5,10 @@ from app.db import Create, register
 register.tag("create", Create)
 
 def home(request):
-    create = {
-        'database': Create(request),
-    }
-    return render(request, "home.html", create)
+    #create = {
+        #'database': Create(request),
+    #}
+    return render(request, "home.html")
 
 def update_data(request):
     return render(request, "update_data.html")
